7cac445eecc5a1846879a3c8272cb4616d49e780
raunak <sauhaarda@gmail.com>
2022-09-22 18:11:57 +0000

    working on generating DAG framework

--- src/olorenchemengine/online.py
+++ src/olorenchemengine/online.py
@@ -1,10 +1,38 @@
-EXCLUDED_WORDS = ["REMOTE_BC_KEY", "args", "kwargs"]
+
+def _is_object(key):
+    return key.split('.')[-1][0].isupper()
+
+
 
 class RemoteSession():
     def __init__(self):
         pass
+
+    def construct(self, object):
+        if hasattr(object, "REMOTE_ID"):
+            return object
+
+        if _is_object(object.REMOTE_BC_KEY):
+            object.REMOTE_ID = generate_random_uuid()
+            print(f"CONSTRUCT {object.REMOTE_ID} => {parameterize_remote(object)}")
+            return object
+        else:
+            raise NotImplementedError("Cannot construct a method or attribute yet!")
+
+    def run_method(self, object, method_name, args, kwargs):
+        if not hasattr(object, "REMOTE_ID"):
+            object = self.construct(object)
+        print(f"RUN {object.REMOTE_ID}.{method_name}")
+
     def __getattribute__(self, key):
-        return RemoteObject(key)
+        EXCLUDED_WORDS = ["run_method", "construct"]
+        if key in EXCLUDED_WORDS:
+            return object.__getattribute__(self, key)
+        return RemoteObject(key, "oce", self)
+
+def generate_random_uuid():
+    import uuid
+    return str(uuid.uuid4())
 
 def parameterize_remote(object):
     if issubclass(type(object), RemoteObject):
@@ -12,6 +40,7 @@ def parameterize_remote(object):
             print("Warning: RemoteObject has no args or kwargs")
             print(f"Object is {object.REMOTE_BC_KEY}")
         return {
+            **({'BC_remote_id': object.REMOTE_ID} if hasattr(object, "REMOTE_ID") else {}),
             **{"BC_class_name": object.REMOTE_BC_KEY.split(".")[-1]},
             **{"args": [parameterize_remote(arg) for arg in object.args]},
             **{"kwargs": {k: parameterize_remote(v) for k, v in object.kwargs.items()}},
@@ -21,7 +50,6 @@ def parameterize_remote(object):
         or issubclass(type(object), int)
         or issubclass(type(object), float)
         or issubclass(type(object), str)
-        or issubclass(type(object), dict)
     ):
         return object
     elif issubclass(type(object), list):
@@ -30,14 +58,18 @@ def parameterize_remote(object):
         raise ValueError(f"Cannot parameterize {object} of type {type(object)}")
 
 class RemoteObject():
-    def __init__(self, key, *args, **kwargs) -> None:
+
+    def __init__(self, key, parent, session) -> None:
         self.REMOTE_BC_KEY = key
+        self.REMOTE_PARENT = parent
+        self.REMOTE_SESSION = session
 
     def __getattribute__(self, key):
+        EXCLUDED_WORDS = ["REMOTE_BC_KEY", "REMOTE_SESSION", "REMOTE_PARENT", "REMOTE_ID", "args", "kwargs"]
         if key in EXCLUDED_WORDS:
             return object.__getattribute__(self, key)
         else:
-            return RemoteObject(self.REMOTE_BC_KEY + "." + key)
+            return RemoteObject(self.REMOTE_BC_KEY + "." + key, self, self.REMOTE_SESSION)
 
     def __repr__(self) -> str:
         if hasattr(self, "args") or hasattr(self, "kwargs"):
@@ -49,12 +81,22 @@ class RemoteObject():
             self.kwargs = kwargs
             return self
         else:
-            return self # TODO: handle calling a function (like train)
+            return self.REMOTE_SESSION.run_method(self.REMOTE_PARENT, self.REMOTE_BC_KEY.split('.')[-1], args, kwargs)
+            # parameterized_kwargs = {parameterize_remote(k): parameterize_remote(v) for k, v in kwargs.items()}
+            # print(f"Calling function {self.REMOTE_BC_KEY.split('.')[-1]} with: args: {[parameterize_remote(a) for a in args]}, kwargs: {parameterized_kwargs} on {parameterize_remote(self.REMOTE_PARENT)}" )
+            # return self # TODO: handle calling a function (like train)
+
+    def __iter__(self):
+        return iter([0, 1])
 
 
 
 
 # TODO: track operators on RemoteObjects
+
+# We are basically making a Python "Compiler" that compiles down to a JSON dictionary - but we can use python
+# in the compiler lol
+
 
 if __name__ == "__main__":
     oce = RemoteSession()
@@ -63,7 +105,11 @@ if __name__ == "__main__":
     model = oce.ensemble.BaseBoosting([
             sub_model1,
             oce.RandomForestModel(oce.OlorenCheckpoint("default"), n_estimators=1000)])
-    # only go to api when function is called!
-    print(parameterize_remote(model))
-    # model.fit(*dataset.train_dataset)
+    model.fit(*dataset.train_dataset)
     # oce.save(model, "model.oce")
+
+
+    # CONSTRUCT model FROM JSON_PARAMS
+    # CONSTRUCT dataset FROM JSON_PARAMS
+    # CONSTRUCT train_dataset FROM dataset.train_dataset
+    # RUN fit on model with train_dataset
